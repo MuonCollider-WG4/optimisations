500ed74cd581a5644d0d9ddfadd2318f69cdbd03
Chris Rogers <chris.rogers@stfc.ac.uk>
2022-05-16 16:08:24 +0100

    Full matching now okay

--- final_cooling_match.py
+++ final_cooling_match.py
@@ -27,9 +27,9 @@ class FitCoils(object):
         self.z1 = 0
         self.pz = pz
         self.lattice = None
-        self.make_fields(0, 0)
+        self.make_fields(0, 0, 0)
 
-    def make_fields(self, match_field_1, match_field_2):
+    def make_fields(self, match_field_1, match_field_2, match_field_3):
         """
         Set up the lattice with some nominal geometry parameters hard coded
         - match_field_1: [T] the field strength of the match coil between the 
@@ -43,17 +43,22 @@ class FitCoils(object):
         field_list = []
         length_m1, rmin_m1, rmax_m1, dz_m1 = 0.5, 0.2, 0.3, 0.5
         length_m2, rmin_m2, rmax_m2, dz_m2 = 0.5, 0.2, 0.3, period/4.0-1.0
+        length_m3, rmin_m3, rmax_m3, dz_m3 = 0.5, 0.2, 0.3, period/4.0-1.5
         for (b0, zcentre, length, rmin, rmax) in [
                 (match_field_1, dz_m1, length_m1, rmin_m1, rmax_m1),
                 (match_field_2, dz_m2, length_m2, rmin_m2, rmax_m2),
+                (match_field_3, dz_m3, length_m3, rmin_m3, rmax_m3),
                 (self.long_field, period/4.0, 19.0, 0.2, 0.3),
-                (self.peak_field, period/4.0, 1.0, 0.1, 0.2),
+                (self.peak_field, period/4.0, 2.0, 0.1, 0.2),
+                (match_field_3, period/2-dz_m3, length_m3, rmin_m3, rmax_m3),
                 (match_field_2, period/2-dz_m2, length_m2, rmin_m2, rmax_m2),
                 (match_field_1, period/2-dz_m1, length_m1, rmin_m1, rmax_m1),
                 (-match_field_1, period/2+dz_m1, length_m1, rmin_m1, rmax_m1),
                 (-match_field_2, period/2+dz_m2, length_m2, rmin_m2, rmax_m2),
+                (-match_field_3, period/2+dz_m3, length_m3, rmin_m3, rmax_m3),
                 (-self.long_field, 3.0*period/4.0, 19.0, 0.2, 0.3),
-                (-self.peak_field, 3.0*period/4.0, 1.0, 0.1, 0.2),
+                (-self.peak_field, 3.0*period/4.0, 2.0, 0.1, 0.2),
+                (-match_field_3, period-dz_m3, length_m3, rmin_m3, rmax_m3),
                 (-match_field_2, period-dz_m2, length_m2, rmin_m2, rmax_m2),
                 (-match_field_1, period-dz_m1, length_m1, rmin_m1, rmax_m1),
             ]:
@@ -64,12 +69,13 @@ class FitCoils(object):
 
     def set_fields_minuit(self):
         """Extract fields from minuit and set them using self.make_fields(...)"""
-        match_field_1, match_field_2 = ROOT.Double(), ROOT.Double()
+        match_field_1, match_field_2, match_field_3 = ROOT.Double(), ROOT.Double(), ROOT.Double()
         err = ROOT.Double()
         self.minuit.GetParameter(0, match_field_1, err)
         self.minuit.GetParameter(1, match_field_2, err)
-        self.make_fields(float(match_field_1), float(match_field_2))
-        return match_field_1, match_field_2
+        self.minuit.GetParameter(2, match_field_3, err)
+        self.make_fields(float(match_field_1), float(match_field_2), float(match_field_3))
+        return match_field_1, match_field_2, match_field_3
 
     def uniform_field_beta(self, z_pos):
         """
@@ -86,15 +92,18 @@ class FitCoils(object):
         to find match_field_2
         """
         # set the z position of the start (z0) and end (z1) of the integration
-        self.z0 = self.lattice.period*1.0/8.0 # half way between peak field and end field
-        self.z1 = self.lattice.period*2.0/8.0 # end of the cell
+        self.z0 = self.lattice.period*2.0/8.0 # peak field
+        self.z1 = self.lattice.period*3.0/8.0 # long solenoid field
         if self.minuit:
-            m1, m2 = self.set_fields_minuit()
+            m1, m2, m3 = self.set_fields_minuit()
         self.minuit = ROOT.TMinuit()
         self.minuit.SetPrintLevel(-1)
-        self.minuit.DefineParameter(0, "match_field_1", m1, 0.5, -10, 10)
-        self.minuit.DefineParameter(1, "match_field_2", -1.0, 0.1, -10, 10)
+        self.minuit.DefineParameter(0, "match_field_1", m1, 0.01, -10, 10)
+        self.minuit.DefineParameter(1, "match_field_2", m2, 0.01, -10, 10)
+        self.minuit.DefineParameter(2, "match_field_3", m3, 0.01, -10, 10)
         self.minuit.FixParameter(0)
+        self.minuit.FixParameter(1)
+        self.minuit.FixParameter(2)
         self.minuit.SetFCN(self.score_function_2)
         self.minuit.Command("SIMPLEX "+str(1000)+" "+str(1e-12))
         beta1 = self.score_function_2(0, 0, [0], 0, 0)
@@ -106,12 +115,12 @@ class FitCoils(object):
         high field region and require uniform beta function at the centre of the
         high field region (should be set to z1) 
         """
-        match_field_1, match_field_2 = self.set_fields_minuit()
+        match_field_1, match_field_2, match_field_3 = self.set_fields_minuit()
         beta0 = self.uniform_field_beta(self.z0)
         beta1_tgt = self.uniform_field_beta(self.z1)
         beta1, dbeta1dz, phi = self.beta_finder.evolve(float(beta0), 0., self.z0, self.z1)
-        score[0] = (beta1-beta1_tgt)**2+(100*dbeta1dz)**2
-        print("Match1: ", match_field_1, "Match2: ", match_field_2,
+        score[0] = (beta1-beta1_tgt)**2+(10*dbeta1dz)**2
+        print("Match1: ", match_field_1, "Match2: ", match_field_2, "Match3: ", match_field_3,
               "beta0", beta0, "beta1", beta1,
               "beta1_tgt", beta1_tgt, "dbeta1dz", dbeta1dz, "score", score[0])
         return beta1, dbeta1dz
@@ -126,10 +135,12 @@ class FitCoils(object):
         self.z1 = self.lattice.period*8.0/8.0 # first peak field
         self.minuit = ROOT.TMinuit()
         self.minuit.SetPrintLevel(-1)
-        self.minuit.DefineParameter(0, "match_field_1", -6.571660086194663, 0.5, -10, 10)
-        self.minuit.DefineParameter(1, "match_field_2", 0, 0.1, -10, 10)
+        self.minuit.DefineParameter(0, "match_field_1", -6.571660086194663,  0.01, -10, 10)
+        self.minuit.DefineParameter(1, "match_field_2",  1.7680346988426603, 0.01, -10, 10)
+        self.minuit.DefineParameter(2, "match_field_3",  5.246053743943765,  0.01, -10, 10)
         self.minuit.FixParameter(0)
         self.minuit.FixParameter(1)
+        self.minuit.FixParameter(2)
         self.minuit.SetFCN(self.score_function_1)
         self.minuit.Command("SIMPLEX "+str(1)+" "+str(1e-12))
         beta1 = self.score_function_1(0, 0, [0], 0, 0)
@@ -141,21 +152,21 @@ class FitCoils(object):
         field flip and require derivative of beta function wrt z at the centre
         of the high field region is 0
         """
-        match_field_1, match_field_2 = self.set_fields_minuit()
+        match_field_1, match_field_2, match_field_3 = self.set_fields_minuit()
         beta0 = self.uniform_field_beta(self.z0)
         beta1, dbeta1dz, phi = self.beta_finder.evolve(float(beta0), 0., self.z0, self.z1)
         score[0] = dbeta1dz**2
-        print("Match1: ", match_field_1, "Match2: ", match_field_2,
+        print("Match1: ", match_field_1, "Match2: ", match_field_2, "Match3: ", match_field_3,
               "beta0", beta0, "beta1", beta1,
               "dbeta1dz", dbeta1dz, "score", score[0])
         return beta1, dbeta1dz
-
 
     def plot_beta(self, beta0, dbetadz0):
         """
         Plot beta as a function of z
         """
         z_list, beta_list, dbetadz_list, phi_list = self.beta_finder.propagate_beta(beta0, dbetadz0, 1001)
+        m1, m2, m3 = self.set_fields_minuit()
         figure = matplotlib.pyplot.figure()
         axes = figure.add_subplot(1, 2, 1)
         axes.plot(z_list, beta_list)
@@ -166,6 +177,7 @@ class FitCoils(object):
         axes.plot(z_list, [self.beta_finder.field(z) for z in z_list])
         axes.set_xlabel("z [m]")
         axes.set_ylabel("B [T]")
+        figure.suptitle("M1={0:.3g} [T]; M2={1:.3g} [T]; M3={2:.3g} [T]".format(m1, m2, m3))
         return figure
 
 def match_fields(field, pz):
@@ -184,10 +196,10 @@ def main():
     pz = 0.2 # GeV/c
     fitter = FitCoils(0.2, 4.0, 0.0)
     beta, dbetadz = fitter.fit_1()
-    fitter.plot_beta(beta, -dbetadz)
     fitter.peak_field = 30.0
     fitter.fit_2()
-    fitter.plot_beta(beta, -dbetadz)
+    figure = fitter.plot_beta(beta, -dbetadz)
+    figure.savefig(os.path.join(plot_dir, "matched_lattice.png"))
     matplotlib.pyplot.show(block=False)
 
 if __name__ == "__main__":
